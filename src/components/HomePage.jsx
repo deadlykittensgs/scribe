2d66f3ecdf4435909ce482eab1f7ec1d0e0fe323
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-05-23 14:30:03 -0500

    working on recording function

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,6 +33,7 @@ try {
     return
     
 }
+setrecordingStatus('recording')
 
 // create new media recorder instance using the stream 
 const media = new MediaRecorder(tempStream,{type: mimeType})
@@ -46,7 +47,36 @@ mediaRecorder.current.ondataavalible =(event) => {
     if (event.data === 0){return}
     localAudioChunks.push(event.data)
 }
+
+setAudioChunks(localAudioChunks)
 }
+
+async function stopRecording() {
+
+  setRecordingStatus('inactive')
+
+  console.log("stop Recording")
+
+mediaRecorder.current.stop()
+mediaRecorder.current.onstop = () => {
+const audioBlob = new Blob(audioChunks, {type: mimeType}) 
+setAudioStream(audioBlob)
+setAudioChunks([])
+
+}
+
+}
+
+
+useEffect( () => {
+  if (recordingStatus === "inactive") {return}
+
+  const interval = setInterval(() => {
+    setDuration(cur => curr + 1)
+  },1000 )
+
+  return () => clearInterval(interval)
+})
  
   return (
     <main className='flex-1 p-4 flex flex-col text-center gap-3 sm:gap-4 md: gap-5 justify-center '>
@@ -58,7 +88,12 @@ mediaRecorder.current.ondataavalible =(event) => {
      className='text-blue-400'>&rarr;</span>Translate</h3>
 
      <button className=' specialBtn px-4 py-2 rounded-xl flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
-        <p className='text-base text-blue-400'>Record</p>
+        <p className='text-base text-blue-400'>{recordingStatus === "inactive" ? 'Record':`Stop Recording` }</p>
+        <div className=' flex items-center gap-2'>
+          {duration && ( <p>
+            {duration}s
+          </p> )}
+        </div>
         <i className="fa-solid fa-microphone-lines"></i>
      </button>
      <p>or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload
